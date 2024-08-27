8308b0e4a5b3c0fb3faedfa7d316352e5faf9cd2
Nwachukwu-Williams <nwachukwuwilliams@outlook.com>
2024-08-27 14:25:37 +0100

    Pharm Authority

---
+++ script.js
@@ -0,0 +1,9 @@
+hambuger.addEventListener("click", () => {
+    hambuger.classList.toggle("active");
+    document.getElementById("pag-nav").classList.toggle("active");
+})
+
+CatMenu.addEventListener("click", () => {
+    CatMenu.classList.toggle("active");
+    document.getElementById("CatList").classList.toggle("active");
+})
