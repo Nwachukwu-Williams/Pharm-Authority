51ef01044dadb6a03a6cb858cf907edc367d5a9c
Nwachukwu-Williams <nwachukwuwilliams@outlook.com>
2024-08-27 14:48:26 +0100

    Pharm Authority

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ CatMenu.addEventListener("click", () => {
     CatMenu.classList.toggle("active");
     document.getElementById("CatList").classList.toggle("active");
 })
+
+
+
+
+
+const intersectionCallback = (entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fadeInUp');
+        }
+    }
+}
+
+const observer = new IntersectionObserver(intersectionCallback);
+
+const items = document.querySelectorAll('.wow');
+for (const item of items) {
+    observer.observe(item);
+}
